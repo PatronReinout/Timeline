86c764aea4f6fdae054a896f4a081fa332ef6c18
PatronReinout <43004404+PatronReinout@users.noreply.github.com>
2019-06-17 13:17:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const slider = document.querySelector(".scroll");
+const slider = document.querySelector(".scroll"); //haalt class uit index
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener("mousedown", e => {
+slider.addEventListener("mousedown", e => { //activeert functie als je muis indrukt
 	isDown = true;
 	slider.classList.add("active");
 	startX = e.pageX - slider.offsetLeft;
 	scrollLeft = slider.scrollLeft;
 });
-slider.addEventListener("mouseleave", () => {
+slider.addEventListener("mouseleave", () => { //activeert functie als je buiten div gaat
 	isDown = false;
 	slider.classList.remove("active");
 });
-slider.addEventListener("mouseup", () => {
+slider.addEventListener("mouseup", () => { //activeert functie als je lost laat
 	isDown = false;
-	slider.classList.remove("active");
+	slider.classList.remove("active"); // zorgt dat de actieve status weggaat
 });
 slider.addEventListener("mousemove", e => {
 	if (!isDown) return;
-	e.preventDefault();
+	e.preventDefault();  //zorgt dat er geen link wordt geoepend 
 	const x = e.pageX - slider.offsetLeft;
-	const walk = (x - startX) * 3; //scroll-fast
+	const walk = (x - startX) * 3; //scroll snelheid
 	slider.scrollLeft = walk;
 	console.log(walk);
 });
